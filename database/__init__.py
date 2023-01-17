c237c6bf790a9ef85c562c7714963e2a368294e3
JonDev2023 <122323517+JonDev2023@users.noreply.github.com>
2023-01-17 13:51:14 -0300

    Create __init__.py

---
+++ database/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+
+database = {}
+
+i = 1
+while i < 100:
+  database[str(i)] = None
+  i += 1
+
+def update(top)
+
+@app.route('/')
+def api():
+  pass
+
+def create(ip):
+  if __name__ == '__main__':
+    app.run(host=ip)
+    print('port: 5000')
