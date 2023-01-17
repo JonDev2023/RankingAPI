ddb52072c2ddae8c5d13bbf223279ffc25355d6c
JonDev2023 <122323517+JonDev2023@users.noreply.github.com>
2023-01-17 14:56:56 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,9 @@
+import requests
+
+def get(database):
+  rqst = requests.get(database)
+  if rqst:
+    return rqst, rqst.json()
+  else:
+    print('Error: The Server Creator make a error or you make a error')
 
